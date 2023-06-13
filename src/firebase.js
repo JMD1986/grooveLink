8a55630c23a0d547beacf358409696261e38015e
John Daugherty <Johndaugherty1986@gmail.com>
2023-06-13 16:41:33 -0500

    think i got firebase hooked up

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -5,4 +14,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
-export default firebaseConfig;
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
